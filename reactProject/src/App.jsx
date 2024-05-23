0b8e97efb5dae444425a82c39ea86729a6cda56d
rominaromani <mariajulia.romani@utec.edu.pe>
2024-05-22 21:42:00 -0500

    Inicializacion del proyecto

---
+++ reactProject/src/App.jsx
@@ -0,0 +1,15 @@
+import {Box} from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Box>
+        Hola Mundo!
+      </Box>
+    </div>
+  )
+}
+
+export default App
