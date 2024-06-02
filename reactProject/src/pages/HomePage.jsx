31b88d4e5caa62e27565d2f12d4dedbbfd090810
rominaromani <mariajulia.romani@utec.edu.pe>
2024-06-01 23:52:35 -0500

    Aside terminado

--- reactProject/src/pages/HomePage.jsx
+++ reactProject/src/pages/HomePage.jsx
@@ -1,12 +1,28 @@
 import { Route, Routes } from "react-router-dom";
 import Aside from "../sections/Aside";
 import router from "../routes/root";
+import { IoIosSearch } from "react-icons/io";
 
 const HomePage = () => {
   return (
     <main className="flex">
       <Aside />
-      <section className="h-screen w-4/5 px-8 overflow-y-auto">
+      <section className="flex flex-col h-screen w-4/5 px-8 overflow-y-auto">
+        <div className="flex items-center justify-between mt-8 mb-4">
+          <div className="flex items-center gap-4 flex-1">
+            <div className="relative flex items-center flex-1">
+              <IoIosSearch className="absolute left-4 text-gray-400" />
+              <input
+                type="text"
+                placeholder="Buscar..."
+                className="pl-10 flex-1 h-8 px-4 rounded-md border border-cach-l2 focus:outline-none"
+              />
+            </div>
+            <button className="bg-cach-l2 text-white px-4 py-1 rounded-md">
+              Buscar
+            </button>
+          </div>
+        </div>
         <Routes>
           {router.map((route) => (
             <Route
